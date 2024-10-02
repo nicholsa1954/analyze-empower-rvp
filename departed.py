49a13f50a675fad71ce0a67e9616fb5a5e091ddd
Alan Nichols <nichols.alan@gmail.com>
2024-10-02 07:18:22 -0500

    Refactoring.  Moved the departed organizers' names and eids to a separate file.

---
+++ departed.py
@@ -0,0 +1,58 @@
+departed_eids = [\
+'u-56-3016',  # Al Nichols
+'u-56-18093', # Fabi,
+'u-56-21557', # Jake Pena
+'sZONfFaf3OOHvz', # Gilbert Nunez
+'idKoO5r7j1gFxv', # Emily Firlinger
+'rboft4TAXwHySN', # Jennifer Estrada
+'Ya6XAA9GBhi8Kl', # Dummy Account
+'bqUNmQHKI3w5AZ', # Ray Pugh
+'FAzpkk9IzXrbt6', # Clay Perry
+'u-56-21623',     # Allison Vasquez-Lovell
+'1oRrlB5DWlbmxz', # Jose Rivera
+'m4pozco9m8exul', # Ruben Ramos
+'TBNKT6PmgtvmOB', # Minerva Cornejo
+'zwrKe8ICrcIQB2', # Alex Arellano
+'2HAXLm7OCK8VHC', # Luam Rincon
+'c9r65cbijsca55', # Emily Torres Luevano
+'k03oyt4ddgd9au', # Vicky Calderon
+'P0CWy9vUnIHhkO', # Leslie Flores
+'t21hvz9jdl15vt', # Ricardo Torres
+'u-56-5837',      # Angel Sanchez
+'5esYyfuBwTApq8', # Raquel Alvarado
+'fncmu10iwxbded', # Stephanie Salgado Altimarano
+'c-167138',       # Ania Jimenez
+'u-56-1841',      # Liam Gonzalez
+'vIHtcC9AjNewEl', # the Unknown Vocero
+'u-56-19305',     # Cassandra Casas
+'u-56-21499'] #Yesenia Perez
+
+
+departed_names = [\
+'Alan Nichols',  # Al Nichols
+'Fabian Maldonado', # Fabi,
+'Jake Pena', # Jake Pena
+'Gilbert Nunez', # Gilbert Nunez
+'Emily Firlinger', # Emily Firlinger
+'Jennifer Estrada', # Jennifer Estrada
+'Dummy Account', # Dummy Account
+'Ray Pugh', # Ray Pugh
+'Clay Perry', # Clay Perry
+'Allison Vasquez-Lovell',     # Allison Vasquez-Lovell
+'Jose Rivera', # Jose Rivera
+'Ruben Ramos', # Ruben Ramos
+'Minerva Cornejo', # Minerva Cornejo
+'Alex Arellano', # Alex Arellano
+'Luam Rincon', # Luam Rincon
+'Emily Torres Luevano', # Emily Torres Luevano
+'Vicky Calderon', # Vicky Calderon
+'Leslie Flores', # Leslie Flores
+'Ricardo Torres',
+'Angel Sanchez',
+'Raquel Alvarado',
+'Stephanie Salgado Altimarano',
+'Ania Jimenez',
+'Liam Gonzalez',
+'the Unknown Vocero',
+'Cassandra Casas',
+'Yesenia Perez' ]
